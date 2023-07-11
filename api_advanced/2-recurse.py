6c19d33919404c76f6466e783be52774c7a5d33c
henriettetuombe <116841112+henriettetuombe@users.noreply.github.com>
2023-07-11 21:05:57 +0000

    api advanced

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" 2-recurse.py """
+""" a recursive function that queries the Reddit API """
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """ returns list with titles of all hot articles in a subreddit """
+    """ returns list with titles """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'after': after}
     response = requests.get(
-        url,
-        headers=headers,
-        params=params,
-        allow_redirects=False
-    )
+                                url,
+                                headers=headers,
+                                params=params,
+                                allow_redirects=False
+                            )
     if response.status_code == 200:
         data = response.json().get('data')
         if data is not None:
