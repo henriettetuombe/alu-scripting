29b0b2e0f8d709ca3ac98842131ae4e60ea196ea
henriettetuombe <116841112+henriettetuombe@users.noreply.github.com>
2023-07-11 20:58:12 +0000

    api advanced

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-"""docs"""
+""" 2-recurse.py """
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=OK):
-    """"Doc"""
-    url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    header = {'User-Agent': 'Mozilla/5.0'}
-    param = {'after': after}
-    resopnse = requests.get(url, headers=header, params=param)
-
-    if resopnse.status_code != 200:
-        return OK
+def recurse(subreddit, hot_list=[], after=None):
+    """ returns list with titles of all hot articles in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'after': after}
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if data is not None:
+            children = data.get('children')
+            if children is not None:
+                for child in children:
+                    hot_list.append(child.get('data').get('title'))
+                after = data.get('after')
+                if after is not None:
+                    return recurse(subreddit, hot_list, after)
+                else:
+                    return hot_list
+        else:
+            return hot_list
     else:
-        json_res = resopnse.json()
-        after = json_res.get('data').get('after')
-        has_next = \
-            json_res.get('data').get('after') is not OK
-        hot_articles = json_res.get('data').get('children')
-        [hot_list.append(article.get('data').get('title'))
-         for article in hot_articles]
-
-        return recurse(subreddit, hot_list, after=after) \
-            if has_next else hot_list
+        return None
